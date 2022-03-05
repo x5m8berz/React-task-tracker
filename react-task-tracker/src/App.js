0c4d34ba7130439b4a757ab1d81b67de4f7eabb9
Hanrocky <hanrocky.halim@gmail.com>
2022-03-05 20:13:51 +0700

    My Second Commit
    This is my second commit
    Added 2 component
    Added 3 function

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,14 +1,63 @@
 // Components
 
 import Header from "./components/Header";
-// import Button from "./components/Button";
-
+import Tasks from "./components/Tasks";
+import { useState } from 'react'
+import AddTask from "./components/AddTask"
 
 //Main app function
 function App() {
+  const [showAddTask,setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appoinment',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Metting at School',
+        day: 'Feb 6th 1:30 pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: false,
+    },
+
+]) 
+
+
+  // Detele Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => task.id === id ? 
+      {...task, reminder: !task.reminder} : task))
+  }
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1 
+    const newTask = { id,...task, }
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? 
+      <Tasks tasks={tasks} 
+      onDelete={deleteTask} 
+      onToggle={toggleReminder} /> : 'No Tasks'}
     </div>
   );
 }
--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,37 +1,18 @@
-const task = [
-    {
-        id: 1,
-        text: 'Doctors Appoinment',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Metting at School',
-        day: 'Feb 6th 1:30 pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30 pm',
-        reminder: false,
-    },
+import Task from "./Task"
 
-]
 
-const Tasks = () => {
-
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
-
         <>
+         {tasks.map((task) => (
+         <Task key={task.id} 
+         task={task} 
+         onDelete={onDelete} 
+         onToggle={onToggle} />
+         ))}
+
         </>
-
-
     )
-
-
-
 }
 
 export default Tasks
